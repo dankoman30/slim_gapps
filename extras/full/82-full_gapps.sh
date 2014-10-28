2237f896879f29cd77f07f70c0bfc4a93bfb02c3
dankoman <dankoman30@users.noreply.github.com>
2014-10-28 13:38:34 +0000

    Inbox: add initial TRDS version 1.0(78094151), add lib, update full gapps backup script and build script to reflect addition of Inbox

--- extras/full/82-full_gapps.sh
+++ extras/full/82-full_gapps.sh
@@ -20,6 +20,7 @@ app/GoogleHome.apk
 app/GoogleTTS.apk
 app/Hangouts.apk
 app/HangoutsDialer.apk
+app/Inbox.apk
 app/Keep.apk
 app/LatinIME.apk
 app/Translate.apk
@@ -65,6 +66,7 @@ lib/libgoogle-ocrclient.so
 lib/libgoogle_recognizer_jni_l.so
 lib/libimageutils-new-v7a.so
 lib/libimageutils-new.so
+lib/libindexeddb.so
 lib/libjgcastservice.so
 lib/libjni_latinime.so
 lib/libjni_t13n_shared_engine.so
