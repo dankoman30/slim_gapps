5b17c4a344a601a2d21c3b2bf692c5226ed17083
jenkins <jenkins@ac0b9bfd-a931-4c07-8adc-108dd3bb0878>
2014-12-03 13:17:15 +0000

    tag for automatic jenkins build jenkins-buildGapps-400

---
+++ tags/jenkins-buildGapps-400/extras/full/82-full_gapps.sh
@@ -0,0 +1,192 @@
+#!/sbin/sh
+# 
+# /system/addon.d/70-gapps.sh
+#
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+addon.d/82-full_gapps.sh
+app/CalendarGoogle/CalendarGoogle.apk
+app/Chrome/Chrome.apk
+app/Chrome/lib/arm/libchrome.1847.114.so
+app/Chrome/lib/arm/libchrome.1916.122.so
+app/Chrome/lib/arm/libchrome.1916.138.so
+app/Chrome/lib/arm/libchrome.1916.141.so
+app/Chrome/lib/arm/libchrome.1985.122.so
+app/Chrome/lib/arm/libchrome.1985.128.so
+app/Chrome/lib/arm/libchrome.1985.131.so
+app/Chrome/lib/arm/libchrome.1985.135.so
+app/Chrome/lib/arm/libchrome.2062.117.so
+app/Chrome/lib/arm/libchrome.2125.102.so
+app/Chrome/lib/arm/libchrome.2125.114.so
+app/Chrome/lib/arm/libchrome.2171.59.so
+app/Chrome/lib/arm/libchromeview.so
+app/Chrome/lib/arm/libchromium_android_linker.so
+app/ConfigUpdater/ConfigUpdater.apk
+app/Drive/Drive.apk
+app/Drive/lib/arm/libdocscanner_image.so
+app/Drive/lib/arm/libdocsimageutils.so
+app/Drive/lib/arm/libfoxit.so
+app/Drive/lib/arm/libparcel_utils.so
+app/Drive/lib/arm/librectifier.so
+app/ExchangeServices/ExchangeServices.apk
+app/Gmail2/Gmail2.apk
+app/GoogleCalendarSyncAdapter/GoogleCalendarSyncAdapter.apk
+app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
+app/GoogleHome/GoogleHome.apk
+app/GoogleTTS/GoogleTTS.apk
+app/GoogleTTS/lib/arm/libpatts_engine_jni_api.so
+app/GoogleTTS/lib/arm/libspeexwrapper.so
+app/Hangouts/Hangouts.apk
+app/Hangouts/lib/arm/libcrashreporter.so
+app/Hangouts/lib/arm/libvideochat_jni.so
+app/HangoutsDialer/HangoutsDialer.apk
+app/Keep/Keep.apk
+app/Keyboard/Keyboard.apk
+app/Keyboard/lib/arm/libjni_unbundled_latinimegoogle.so
+app/Maps/Maps.apk
+app/Maps/lib/arm/libcrashreporter.so
+app/Maps/lib/arm/libgmm-jni.so
+app/Messenger/Messenger.apk
+app/Music2/Music2.apk
+app/PlusOne/PlusOne.apk
+app/PlusOne/lib/arm/libcrashreporter.so
+app/PlusOne/lib/arm/libcronet.so
+app/PlusOne/lib/arm/libfilterframework_jni.so
+app/PlusOne/lib/arm/libmoviemaker-jni.so
+app/PlusOne/lib/arm/libphotoeditor_native.so
+app/PlusOne/lib/arm/libwebp_android.so
+app/Street/Street.apk
+app/Translate/Translate.apk
+app/Translate/lib/arm/libclientvision-new-v7a.so
+app/Translate/lib/arm/libclientvision-new.so
+app/Translate/lib/arm/libgoggles_clientvision-v7a.so
+app/Translate/lib/arm/libgoggles_clientvision.so
+app/Translate/lib/arm/libgoggles_imageutils-v7a.so
+app/Translate/lib/arm/libgoggles_imageutils.so
+app/Translate/lib/arm/libimageutils-new-v7a.so
+app/Translate/lib/arm/libimageutils-new.so
+app/Translate/lib/arm/libzxing-new-v7a.so
+app/Translate/lib/arm/libzxing-new.so
+app/Videos/Videos.apk
+app/Videos/lib/arm/libWVphoneAPI.so
+app/Wallet/Wallet.apk
+app/Wallet/lib/arm/libgoogle-ocrclient.so
+app/YouTube/YouTube.apk
+app/YouTube/lib/arm/libcronet.so
+app/YouTube/lib/arm/libm2ts_player.so
+etc/g.prop
+etc/permissions/com.google.android.camera2.xml
+etc/permissions/com.google.android.maps.xml
+etc/permissions/com.google.android.media.effects.xml
+etc/permissions/com.google.widevine.software.drm.xml
+etc/permissions/features.xml
+etc/preferred-apps/google.xml
+framework/com.google.android.camera2.jar
+framework/com.google.android.maps.jar
+framework/com.google.android.media.effects.jar
+framework/com.google.widevine.software.drm.jar
+lib/libfilterpack_facedetect.so
+priv-app/GoogleBackupTransport/GoogleBackupTransport.apk
+priv-app/GoogleFeedback/GoogleFeedback.apk
+priv-app/GoogleLoginService/GoogleLoginService.apk
+priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
+priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
+priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
+priv-app/Phonesky/Phonesky.apk
+priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk
+priv-app/PrebuiltGmsCore/lib/arm/libAppDataSearch.so
+priv-app/PrebuiltGmsCore/lib/arm/libWhisper.so
+priv-app/PrebuiltGmsCore/lib/arm/libconscrypt_gmscore_jni.so
+priv-app/PrebuiltGmsCore/lib/arm/libgames_rtmp_jni.so
+priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_base.so
+priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_support.so
+priv-app/PrebuiltGmsCore/lib/arm/libgms-ocrclient.so
+priv-app/PrebuiltGmsCore/lib/arm/libgmscore.so
+priv-app/PrebuiltGmsCore/lib/arm/libjgcastservice.so
+priv-app/PrebuiltGmsCore/lib/arm/libsslwrapper_jni.so
+priv-app/SetupWizard/SetupWizard.apk
+priv-app/Velvet/Velvet.apk
+priv-app/Velvet/lib/arm/libgoogle_hotword_jni.so
+priv-app/Velvet/lib/arm/libgoogle_recognizer_jni_l.so
+priv-app/Velvet/lib/arm/libvcdecoder_jni.so
+priv-app/talkback/talkback.apk
+tts/lang_pico/de-DE_gl0_sg.bin
+tts/lang_pico/de-DE_ta.bin
+tts/lang_pico/es-ES_ta.bin
+tts/lang_pico/es-ES_zl0_sg.bin
+tts/lang_pico/fr-FR_nk0_sg.bin
+tts/lang_pico/fr-FR_ta.bin
+tts/lang_pico/it-IT_cm0_sg.bin
+tts/lang_pico/it-IT_ta.bin
+usr/srec/en-US/c_fst
+usr/srec/en-US/clg
+usr/srec/en-US/commands.abnf
+usr/srec/en-US/compile_grammar.config
+usr/srec/en-US/contacts.abnf
+usr/srec/en-US/dict
+usr/srec/en-US/dictation.config
+usr/srec/en-US/dnn
+usr/srec/en-US/endpointer_dictation.config
+usr/srec/en-US/endpointer_voicesearch.config
+usr/srec/en-US/ep_acoustic_model
+usr/srec/en-US/g2p_fst
+usr/srec/en-US/grammar.config
+usr/srec/en-US/hclg_shotword
+usr/srec/en-US/hmm_symbols
+usr/srec/en-US/hmmlist
+usr/srec/en-US/hotword.config
+usr/srec/en-US/hotword_classifier
+usr/srec/en-US/hotword_normalizer
+usr/srec/en-US/hotword_prompt.txt
+usr/srec/en-US/hotword_word_symbols
+usr/srec/en-US/metadata
+usr/srec/en-US/norm_fst
+usr/srec/en-US/normalizer
+usr/srec/en-US/offensive_word_normalizer
+usr/srec/en-US/phone_state_map
+usr/srec/en-US/phonelist
+usr/srec/en-US/rescoring_lm
+usr/srec/en-US/wordlist
+app/FaceLock/FaceLock.apk
+app/FaceLock/lib/arm/libfacelock_jni.so
+vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/landmark_group_meta_data.bin
+vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin
+vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin
+vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-3-tree7-wmd.bin
+vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-r0-ri30.4a-v24-tree7-2-wmd.bin
+vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rn30-ri30.5-v24-tree7-2-wmd.bin
+vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rp30-ri30.5-v24-tree7-2-wmd.bin
+vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-r.8.1.bin
+vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-y-r.8.1.bin
+vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
