210980a12c772776770f69cee649235b500cfc7c
Daniel Koman <dankoman30@gmail.com>
2015-03-11 09:57:18 -0600

    Messenger 1.2.036 (1773886-38)

--- extras/full/80-gapps.sh
+++ extras/full/80-gapps.sh
@@ -55,6 +55,8 @@ app/Maps/Maps.apk
 app/Maps/lib/arm/libcrashreporterer.so
 app/Maps/lib/arm/libgmm-jni.so
 app/Messenger/Messenger.apk
+app/Messenger/lib/arm/libframesequence.so
+app/Messenger/lib/arm/libgiftranscode.so
 app/Music2/Music2.apk
 app/PlusOne/PlusOne.apk
 app/PlusOne/lib/arm/libcrashreporterer.so
