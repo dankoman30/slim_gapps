72e659563f0947193a467fbbca559f3548cad3a2
uzen <uzen134@gmail.com>
2015-05-01 04:23:40 +0500

    all packets are separated into individual plugins

---
+++ README.md
@@ -0,0 +1,19 @@
+# Slim_gapps
+
+<b>Dependencies</b>: <i>java-environment, apache-ant</i>
+
+<b>How to use?</b>
+
+run <b>ant</b> from the root directory
+
+<a href="https://github.com/dankoman30/slimlp_gapps">Download Slim GApps</a>
+
+<b>Gapps packages can be downloaded here:</b>
+
+<a href="http://104.236.22.120/index.php?device=gapps/lollipop">mirror1</a>
+
+<a href="http://209.95.39.164/Gapps/Lollipop/">mirror2</a>
+
+
+
+<a href="http://forum.xda-developers.com/slimroms/general/gapps-official-slim-gapps-trds-slimkat-t2792842">General Discussion</a>
---
+++ structure/optional/faceunlock/install-face.sh
@@ -0,0 +1,67 @@
+#!/sbin/sh
+if (grep -qi "mako" /proc/cpuinfo ); then
+  echo "Installing Mako-specific google bits"
+  cp -a /tmp/common/* /system/
+fi
+
+if (grep -qi "hammerhead" /proc/cpuinfo ); then
+  echo "Installing Hammerhead-specific google bits"
+  cp -a /tmp/common/* /system/
+  cp -a /tmp/hammerhead/* /system/
+fi
+
+if (grep -qi "shamu" /proc/cpuinfo ); then
+  echo "Installing Shamu-specific google bits"
+  cp -a /tmp/common/* /system/
+  cp -a /tmp/shamu/* /system/
+fi
+
+if (grep -qi "manta" /proc/cpuinfo ); then
+  echo "Installing Manta-specific google bits"
+  cp -a /tmp/common/* /system/
+  cp -a /tmp/manta/* /system/
+fi
+
+if (grep -qi "flo" /proc/cpuinfo ); then
+  echo "Installing Razor-specific google bits"
+  cp -a /tmp/common/* /system/
+fi
+
+if (grep -qi "deb" /proc/cpuinfo ); then
+  echo "Installing Razor-specific google bits"
+  cp -a /tmp/common/* /system/
+fi
+
+if (grep -qi "tuna" /proc/cpuinfo ); then
+  echo "Installing Tuna-specific google bits"
+  cp -a /tmp/common/* /system/
+  cp -a /tmp/tuna/* /system/
+fi
+
+good_ffc_device() {
+  if [ -f /sdcard/.forcefaceunlock ]; then
+    return 0
+  fi
+  if cat /proc/cpuinfo |grep -q Victory; then
+    return 1
+  fi
+  if cat /proc/cpuinfo |grep -q herring; then
+    return 1
+  fi
+  if cat /proc/cpuinfo |grep -q sun4i; then
+    return 1
+  fi
+  return 0
+}
+
+if good_ffc_device && [ -e /system/etc/permissions/android.hardware.camera.front.xml ]; then
+  echo "Installing face detection support"
+  cp -a /tmp/face/* /system/
+  chmod 755 /system/addon.d/71-gapps-faceunlock.sh
+elif  [ -d /system/vendor/pittpatt/ ]; then
+  rm -rf /system/vendor/pittpatt/
+  rm  -f /system/app/FaceLock.apk
+  rm  -f /system/lib/libfacelock_jni.so
+  rm  -f /system/addon.d/71-gapps-faceunlock.sh
+fi
+rm -rf /tmp/face
