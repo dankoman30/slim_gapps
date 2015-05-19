a8189430692944b218fde6cb0da0d6b77078520f
Daniel Koman <dankoman30@gmail.com>
2015-05-19 14:07:13 -0400

    use kilobytes instead of bytes to avoid bash out of range errors

--- extras/space.sh
+++ extras/space.sh
@@ -2,8 +2,8 @@
 
 # vars
 g_prop=/system/etc/g.prop
-current_gapps_size=0
-buffer=100000000
+current_gapps_size_kb=0
+buffer_kb=100000
 force_install=0
 # STATUS for use in edify (1=not enough space, 2=non-slim gapps installed,
 # 3=rom not installed, 10=force install)
@@ -28,12 +28,12 @@ OUTFD=$(ps | grep -v grep | grep -oE "update-binary(.*)" | cut -d " " -f 3)
 # outputs an empty line
 
 ui_print() {
-  if [ "$OUTFD" != "" ]; then
-    echo "ui_print ${1} " 1>&$OUTFD;
-    echo "ui_print " 1>&$OUTFD;
-  else
-    echo "${1}";
-  fi;
+    if [ "$OUTFD" != "" ]; then
+        echo "ui_print ${1} " 1>&$OUTFD;
+        echo "ui_print " 1>&$OUTFD;
+    else
+        echo "${1}";
+    fi;
 }
 
 file_getprop() {
@@ -47,7 +47,7 @@ then
     then
         # and it is a recent slim gapps package
         ui_print "  found existing slim gapps package"
-        current_gapps_size=$(file_getprop $g_prop ro.addon.size)
+        current_gapps_size_kb=$(((file_getprop $g_prop ro.addon.size) / 1024))
     else
         special_status 2
     fi
@@ -66,25 +66,22 @@ else
 fi
 
 # Read and save system partition size details
-#df=$(busybox df /system | tail -n 1)
 df=$(df /system | tail -n 1)
 case $df in
     /dev/block/*) df=$(echo $df | awk '{ print substr($0, index($0,$2)) }');;
 esac
-# NEED TO MULTIPLY BY 1000 BECAUSE THIS VALUE IS IN 1K BLOCKS
 free_system_size_kb=$(echo $df | awk '{ print $3 }')
-free_system_size=$(($free_system_size_kb \* 1000))
 
 # add currently installed gapps size (since they will be removed)
 # and subtract buffer size to get actual available system space
-post_free_system_size=$(($free_system_size + $current_gapps_size - $buffer))
-needed_system_size=@space_needed@
+post_free_system_size_kb=$((free_system_size_kb + current_gapps_size_kb - buffer_kb))
+needed_system_size_kb=$((@space_needed@ / 1024))
 
-ui_print "system space before gapps removal: $free_system_size"
-ui_print "system space after gapps removal: $post_free_system_size"
-ui_print "system space required: $needed_system_size"
-ui_print "current gapps size: $current_gapps_size"
-if [ "$post_free_system_size" -ge $needed_system_size ]
+ui_print "system space before gapps removal: $free_system_size_kb KB"
+ui_print "system space after gapps removal minus buffer: $post_free_system_size_kb KB"
+ui_print "system space required: $needed_system_size_kb KB"
+ui_print "current gapps size: $current_gapps_size_kb KB"
+if [ "$post_free_system_size_kb" -ge $needed_system_size_kb ]
 then
     # CONTINUE TO INSTALL GAPPS
     ui_print "gapps installation will now proceed..."
