e60558975606e9680a87416e6c485ddb2db54aab
uzen <uzen134@gmail.com>
2015-10-04 00:20:01 +0500

    dynamic restructuring [2] slimapk.jar ver. 1.1.3 move HotwordEnrollment -> optional/flounder

--- structure/dynamic.sh
+++ structure/dynamic.sh
@@ -3,52 +3,19 @@
 # Variables
 ARCH=$(grep ro.product.cpu.abi= /system/build.prop | cut -d "=" -f 2);
 DEVICE=$(grep ro.product.device= /system/build.prop | cut -d "=" -f 2);
+DYNAMIC="arm"
+ANY="any"
 
-# HotwordEnrollment
-if [ $DEVICE == flounder ]; then
- cp -af /tmp/HotwordEnrollment/arm64/* /system
-fi
-
-if [ $DEVICE == Flounder ]; then
- cp -af /tmp/HotwordEnrollment/arm64/* /system
-fi
-
-# PrebuiltGmsCore
-# IF 64-BIT ARCHITECTURE INSTALL 240
 if [ $ARCH == arm64-v8a ]; then
- cp -af /tmp/PrebuiltGmsCore/240/* /system
- cp -af /tmp/PrebuiltGmsCore/arm64/* /system
-else
-# COPY 230 (ARM VERSION) TO SYSTEM
- cp -af /tmp/PrebuiltGmsCore/230/* /system
- cp -af /tmp/PrebuiltGmsCore/arm/* /system
+ $DYNAMIC="arm64"
 fi
 
-# Swypelib
-if [ $ARCH == armeabi-v7a ]; then
- cp -af /tmp/Swypelib/lib/* /system/lib
-elif [ $ARCH == arm64-v8a ]; then
- cp -af /tmp/Swypelib/lib64/* /system/lib64
-else
- cp -af /tmp/Swypelib/lib/* /system/lib
-fi
+# COPY Applications TO SYSTEM
+cp -af /tmp/$DYNAMIC/* /system
 
-# FaceLock #mini
-cp -af /tmp/FaceLock/vendor/* /system/vendor #mini
-if [ $ARCH == armeabi-v7a ]; then #mini
- cp -af /tmp/FaceLock/arm/* /system #mini
-elif [ $ARCH == arm64-v8a ]; then #mini
- cp -af /tmp/FaceLock/arm64/* /system #mini
-else #mini
- cp -af /tmp/FaceLock/arm/* /system #mini
-fi #mini
-#mini
-# Velvet #mini
-cp -af /tmp/Velvet/usr/* /system/usr #mini
-if [ $ARCH == armeabi-v7a ]; then #mini
- cp -af /tmp/Velvet/arm/* /system #mini
-elif [ $ARCH == arm64-v8a ]; then #mini
- cp -af /tmp/Velvet/arm64/* /system #mini
-else #mini
- cp -af /tmp/Velvet/arm/* /system #mini
-fi #mini
+# COPY Additional files
+for DIR in $(find /tmp/storage -maxdepth 1 -type d); 
+do
+	cp -af $DIR/$DYNAMIC/* /system 2>/dev/null 
+	cp -af $DIR/$ANY/* /system 2>/dev/null 
+done
--- structure/install-optional.sh
+++ structure/install-optional.sh
@@ -1,8 +1,14 @@
 #!/sbin/sh
 
-device=$(grep -io '\(mako\|hammerhead\|shamu\|manta\|flo\|deb\)' /proc/cpuinfo)
+device=$((grep -io '\( mako$\| hammerhead$\| shamu$\| manta$\| flo$\| deb$\| volantis$\| flounder$\)' /proc/cpuinfo)|awk '{print tolower($0)}')
 
 if [ $device ]; then
+
+if [ $device = "flounder" -o  $device = "volantis" ]; then
+  cp -af /tmp/flounder/* /system/
+  exit
+fi
+
 echo "Installing specific google bits"
 cp -a /tmp/common/* /system/
 
