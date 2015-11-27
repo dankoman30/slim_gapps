f1898e4393de669977232ee552c9170c0a48d54b
Daniel Koman <dankoman30@gmail.com>
2015-11-27 14:03:45 +0100

    authorship

--- tools/libWork.sh
+++ tools/libWork.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#This file is part of The Open GApps script of @mfonville.
+#
+#    The Open GApps scripts are free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    These scripts are distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+
 # define variables
 targetdir="$1" #use apk directory as parameter
 targetapk="$targetdir/$(basename "$targetdir").apk"
