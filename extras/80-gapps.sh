6b2771bac83dc120dda11a84a765c23e8d960d38
Daniel Koman <dankoman30@gmail.com>
2015-12-10 10:45:31 -0500

    add dynamic files to backup script at build time

--- extras/80-gapps.sh
+++ extras/80-gapps.sh
@@ -1,62 +1,18 @@
 #!/sbin/sh
 # 
-# /system/addon.d/70-gapps.sh
+# /system/addon.d/80-gapps.sh
 #
+
+# Execute
 . /tmp/backuptool.functions
+
+# Variables
+ARCH=`grep ro.product.cpu.abi= /system/build.prop | cut -d "=" -f 2`
 
 list_files() {
 cat <<EOF
 addon.d/80-gapps.sh
 etc/g.prop
-etc/permissions/com.google.android.camera2.xml
-framework/com.google.android.camera2.jar
-lib/libgcam.so
-lib/libgcam_swig_jni.so
-lib/libjni_eglfence.so
-lib/liblightcycle.so
-lib/libnativehelper_compat.so
-app/FaceLock/FaceLock.apk
-app/FaceLock/lib/arm/libfacelock_jni.so
-app/FaceLock/lib/arm64/libfacelock_jni.so
-lib/libfilterpack_facedetect.so
-lib/libjni_latinime.so
-lib/libjni_latinimegoogle.so
-lib64/libfilterpack_facedetect.so
-lib64/libjni_latinime.so
-lib64/libjni_latinimegoogle.so
-priv-app/HotwordEnrollment/HotwordEnrollment.apk
-priv-app/PrebuiltGmsCore/lib/arm/libAppDataSearch.so
-priv-app/PrebuiltGmsCore/lib/arm/libconscrypt_gmscore_jni.so
-priv-app/PrebuiltGmsCore/lib/arm/libdirect-audio.so
-priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_base.so
-priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_support.so
-priv-app/PrebuiltGmsCore/lib/arm/libgmscore.so
-priv-app/PrebuiltGmsCore/lib/arm/libgms-ocrclient.so
-priv-app/PrebuiltGmsCore/lib/arm/libjgcastservice.so
-priv-app/PrebuiltGmsCore/lib/arm/libNearbyApp.so
-priv-app/PrebuiltGmsCore/lib/arm/libsslwrapper_jni.so
-priv-app/PrebuiltGmsCore/lib/arm/libwearable-selector.so
-priv-app/PrebuiltGmsCore/lib/arm/libWhisper.so
-priv-app/PrebuiltGmsCore/lib/arm64/libAppDataSearch.so
-priv-app/PrebuiltGmsCore/lib/arm64/libconscrypt_gmscore_jni.so
-priv-app/PrebuiltGmsCore/lib/arm64/libdirect-audio.so
-priv-app/PrebuiltGmsCore/lib/arm64/libgcastv2_base.so
-priv-app/PrebuiltGmsCore/lib/arm64/libgcastv2_support.so
-priv-app/PrebuiltGmsCore/lib/arm64/libgmscore.so
-priv-app/PrebuiltGmsCore/lib/arm64/libgms-ocrclient.so
-priv-app/PrebuiltGmsCore/lib/arm64/libjgcastservice.so
-priv-app/PrebuiltGmsCore/lib/arm64/libNearbyApp.so
-priv-app/PrebuiltGmsCore/lib/arm64/libsslwrapper_jni.so
-priv-app/PrebuiltGmsCore/lib/arm64/libwearable-selector.so
-priv-app/PrebuiltGmsCore/lib/arm64/libWhisper.so
-priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk
-priv-app/Velvet/lib/arm/libcronet.so
-priv-app/Velvet/lib/arm/libgoogle_speech_jni.so
-priv-app/Velvet/lib/arm/libgoogle_speech_micro_jni.so
-priv-app/Velvet/lib/arm64/libcronet.so
-priv-app/Velvet/lib/arm64/libgoogle_speech_jni.so
-priv-app/Velvet/lib/arm64/libgoogle_speech_micro_jni.so
-priv-app/Velvet/Velvet.apk
 @file.list@
 EOF
 }
@@ -95,7 +51,6 @@ case "$1" in
     rm -rf /system/priv-app/QuickSearchBox
 
     # Make required symbolic links
-    ARCH=`grep ro.product.cpu.abi= /system/build.prop | cut -d "=" -f 2`
     if [ $ARCH == armeabi-v7a ]; then
       mkdir -p /system/app/FaceLock/lib/arm #mini
       ln -s /system/lib/libfacelock_jni.so /system/app/FaceLock/lib/arm/libfacelock_jni.so #mini
